bd8c5bb1e2ee3ef4615ce765acd81202bebe23bb
gepan <18582329176@163.com>
2018-11-23 14:07:10 +0800

    addspring

---
+++ spring_boots/src/main/java/com/tecode/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.tecode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Administrator on 2018/11/21.
+ */
+@RestController
+public class UserController {
+    @RequestMapping("/index")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("user","abc");
+        mv.setViewName("list");
+        return mv;
+    }
+}
